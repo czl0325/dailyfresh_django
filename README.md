f2efe940f643c47995c64765f1faa9f907508771
陈昭良 <czl2106082@163.com>
2020-03-10 11:20:24 +0800

    设置全部配置好，生成表成功

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dailyfresh_django
 django开发的天天生鲜项目
+
+
+### 本项目所需要的库
+
+```
+pip install Django              # django框架
+pip install django-tinymce      # 富文本编辑器
+pip install Pillow              # ImageField使用
+pip install PyMySQL             # mysql数据库
+```
---
+++ dailyfresh/apps/user/models.py
@@ -0,0 +1,30 @@
+from db.baseModel import BaseModel
+from django.db import models
+
+
+class User(BaseModel):
+    """用户模型类"""
+    username = models.CharField(max_length=16, verbose_name="用户名")
+    password = models.CharField(max_length=20, verbose_name="密码")
+    mobile = models.CharField(max_length=20, verbose_name="手机号码")
+    email = models.CharField(max_length=50, verbose_name="邮箱")
+
+    class Meta:
+        db_table = "df_user"
+        verbose_name = "用户"
+        verbose_name_plural = verbose_name
+
+
+class Address(BaseModel):
+    """地址模型类"""
+    user = models.ForeignKey('User', verbose_name='所属账户',on_delete=models.CASCADE)
+    receiver = models.CharField(max_length=20, verbose_name='收件人')
+    addr = models.CharField(max_length=256, verbose_name='收件地址')
+    zip_code = models.CharField(max_length=6, null=True, verbose_name='邮政编码')
+    phone = models.CharField(max_length=11, verbose_name='联系电话')
+    is_default = models.BooleanField(default=False, verbose_name='是否默认')
+
+    class Meta:
+        db_table = 'df_address'
+        verbose_name = '地址'
+        verbose_name_plural = verbose_name
